f49af04186e2ded2b685c938bd1779f7cb208db4
Luciano Peña <penaluciano.developer@gmail.com>
2023-08-29 13:26:20 -0300

    Desafio2

---
+++ ProductManager.js
@@ -0,0 +1,161 @@
+const fs = require("fs");
+
+class ProductManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
+
+  fileExist() {
+    return fs.existsSync(this.filePath);
+  }
+
+  async getProducts() {
+    try {
+      if (this.fileExist()) {
+        const contenido = await fs.promises.readFile(this.filePath, "utf-8");
+        const contenidoJson = JSON.parse(contenido);
+        return contenidoJson;
+      } else {
+        throw new Error("No se puede leer el archivo");
+      }
+    } catch (error) {
+      console.log(error.message);
+      throw new Error("Error al obtener.");
+    }
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    try {
+      if (this.fileExist()) {
+        const contenido = await fs.promises.readFile(this.filePath, "utf-8");
+        const contenidoJson = JSON.parse(contenido);
+
+        if (contenidoJson.some((product) => product.code === code)) {
+          throw new Error("Ya existe un producto con el mismo código.");
+        }
+
+        const newProduct = {
+          id: this.generateUniqueId(),
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+        };
+
+        contenidoJson.push(newProduct);
+
+        await fs.promises.writeFile(
+          this.filePath,
+          JSON.stringify(contenidoJson, null, "\t")
+        );
+        console.log("Producto agregado.");
+        return newProduct;
+      } else {
+        throw new Error("No se puede agregar el producto");
+      }
+    } catch (error) {
+      console.log(error.message);
+      throw new Error("Error al agregar.");
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const products = await this.getProducts();
+      const product = products.find((product) => product.id === id);
+      if (!product) {
+        throw new Error("Producto no encontrado.");
+      }
+      return product;
+    } catch (error) {
+      console.error("Error al obtener por ID:", error.message);
+      throw new Error("Error al obtener por ID");
+    }
+  }
+
+  async updateProduct(id, updatedFields) {
+    try {
+      const products = await this.getProducts();
+      const productIndex = products.findIndex((product) => product.id === id);
+      if (productIndex === -1) {
+        throw new Error("No encontrado");
+      }
+      const updatedProduct = {
+        ...products[productIndex],
+        ...updatedFields,
+        id,
+      };
+      products[productIndex] = updatedProduct;
+      await fs.promises.writeFile(
+        this.filePath,
+        JSON.stringify(products, null, "\t")
+      );
+      console.log("Producto actualizado:", updatedProduct);
+      return updatedProduct;
+    } catch (error) {
+      console.error("Error al actualizar producto:", error.message);
+      throw new Error("Error al actualizar producto.");
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      const products = await this.getProducts();
+      const productIndex = products.findIndex((product) => product.id === id);
+      if (productIndex === -1) {
+        throw new Error("Producto no encontrado");
+      }
+      const deletedProduct = products.splice(productIndex, 1)[0];
+      await fs.promises.writeFile(
+        this.filePath,
+        JSON.stringify(products, null, "\t")
+      );
+      console.log("Producto eliminado:", deletedProduct);
+      return deletedProduct;
+    } catch (error) {
+      console.error("Error al eliminar producto:", error.message);
+      throw new Error("Error al eliminar producto.");
+    }
+  }
+
+  generateUniqueId() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+  }
+}
+
+(async () => {
+  try {
+    const manager = new ProductManager("./Products.json");
+
+    const newProduct = await manager.addProduct(
+      "Remera",
+      "Desde talle S a XXL.",
+      2000,
+      "Imagen",
+      "REM213",
+      10
+    );
+    console.log(newProduct);
+
+    const updatedFields = {
+      title: "Producto actualizado",
+      price: 250,
+    };
+    await manager.updateProduct(newProduct.id, updatedFields);
+
+    const deletedProduct = await manager.deleteProduct(newProduct.id);
+    console.log("Producto eliminado:", deletedProduct);
+
+    const products = await manager.getProducts();
+    if (products.length > 0) {
+      const productFound = await manager.getProductById(products[0].id);
+      console.log("Producto encontrado por ID:", productFound);
+    } else {
+      console.log("No hay productos.");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
